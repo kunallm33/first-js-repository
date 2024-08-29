16dd96a332d0c1cf229b0c77371fb98607ccd29c
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-08-29 13:37:07 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,20 @@
+// Primitive datatypes
+
+newstring = 'neww'
+
+number = 46
+number2 = 47.58
+
+nullVar = null
+
+booleanVar   = true
+
+let email
+
+const id = Symbol(123)
+const anotherId = Symbol(123)
+
+console.log(id === anotherId)
+
+// nonprimitive
+

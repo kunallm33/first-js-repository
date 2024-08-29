415d3470f2fab021fbebc91c26d2457f0635dab0
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-08-29 12:54:04 +0000

    data types in js

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+'use strict'    // treat all js code as new version
+
+// aler('39-45')   we are not in the browser to use this command, we are in nodejs
+
+//1) number : it can store the value in the form of float, double,int but not a realy big value
+// 2) bigint : it can only story the interger
+// 3) string: it can store the string values
+// 4) boolean: it can store true or false
+// 5) null: it is basically a empty varialbe
+// 6) undefined: it is shown when we dont initialize the var
+// 7) symbol: unique
+
+console.log(typeof 99)

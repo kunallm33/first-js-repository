d5ab316cea55a03a1d5031487a8c2397bbb89c4d
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-08-29 12:53:36 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const empID = 'newuser'
+let empCity = 'Mumbai'
+var empName = 'Akshay'
+empHT = 'patan'
+let empState
+
+console.log(empID)
+
+// empID = 'olduser' //prefer not use const
+empCity = 'Pune'
+empName = 'ram'
+empHT = 'mhavashi'
+
+console.table([empCity,empName,empHT,empState])

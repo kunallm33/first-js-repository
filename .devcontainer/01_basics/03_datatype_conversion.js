4630be48ca0164fff8f766de5c67898b8fe30d65
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-08-29 12:54:24 +0000

    datatypes conversion

---
+++ .devcontainer/01_basics/03_datatype_conversion.js
@@ -0,0 +1,46 @@
+// let num  = true
+
+// // console.log(typeof num);
+// // console.log(typeof (num));
+
+// // let valueInNumber = Number(num)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
+ 
+// let blnvalue = 0
+// let value = Boolean(blnvalue)
+// console.log(typeof value)
+// console.log(value);
+
+// let val = 33
+// let tostring =  String(val)
+// console.log(tostring)
+// console.log(typeof tostring);
+
+
+/*************************** String to number and number to string ******************************/
+
+// console.log(3+44);
+// console.log(3+'44');
+// console.log('3'+44);
+// console.log('3'+'44');
+// console.log(3 + '4'+ 3);
+// console.log('3' + '4'+ 3);
+// console.log(3 + 4+ '3');
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+'');
+
+// let a,b,c
+// a=b=c=4+3
+// console.log(b) //would be 7 for three of them
+
+let gameCounter = 100
+let other = gameCounter++
+console.log(other);
+
+
+
+
+

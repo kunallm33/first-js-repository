befac36c230d5f9a130780261c850c5336ccd443
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-09-02 13:41:11 +0000

    Arrays

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+let arr  = [1,2,3,4,5]
+
+let arr1 = ['kksks','res',23, '$' ]
+
+// console.log(arr1)
+
+let arr2  = new Array(1,2,3,'kunal')
+
+// console.log(arr2);
+// console.log(arr2[2]);
+
+// newarr = ['amar',2,5,2,'wer']
+
+// newarr.push(7)
+// console.log(newarr);
+// newarr.pop()
+// console.log(newarr);
+
+another_arr = [3,52,'fgg',5,65]
+// another_arr.unshift(8)
+// console.log(another_arr);
+// another_arr.shift()
+// console.log(another_arr);
+
+// console.log(another_arr.includes(3));
+// console.log(another_arr.indexOf(52));
+
+// another_arr = arr2.join()
+// console.log(arr2);
+// console.log(typeof arr2);
+
+// let __ = another_arr.slice(0,3)
+// console.log(__);
+
+// const months = ['Jan', 'March', 'April', 'June'];
+
+// let splice_user = months.splice(1,0,'feb')
+// console.log(months);
+
+// let splice_user2 = months.splice(4,0,'may')
+// console.log(months);
+
---
+++ .devcontainer/02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+// let marvel = ['spiderman','captain','hulk']
+
+// let dc =  ['superman','batman', 'flash']
+
+// // marvel.push(dc)
+// // allheroes = marvel.concat(dc)
+
+// allheroes = [...marvel,...dc]
+// console.log(allheroes);
+
+
+// let newarr = [1,3,4,5,6,8,[2,3,5,5],34,52,[3,3,2,11,5,[23,22],34],2,3,42]
+
+// let sortedarr  = newarr.flat(Infinity) //enter the depth of array
+
+// console.log(sortedarr);
+
+console.log(Array.isArray('dhd','dhed'));
+console.log(Array.from('dhed'));
+
+console.log(Array.from({name:'harry'}));    //from cant fetch the key value pairs
+
+let guy1 = 'hitesh'
+let guy2 = 'harry'
+let guy3 = 'sunny'
+
+console.log(Array.of(guy1,guy2,guy3));

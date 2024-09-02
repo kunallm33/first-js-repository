cf2b9e2ba37bb1a976cfd8ad0860ba7a4790ad25
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-09-02 11:58:20 +0000

    dates

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,18 @@
+// Dates
+
+let md = new Date()
+
+// console.log(md.toDateString())
+// console.log(md.toString())
+// console.log(md.toLocaleString())
+
+let new_md = new Date(2023,11,23)
+// console.log(new_md.toDateString())
+// console.log(new_md.toString())
+// console.log(new_md.toLocaleDateString())
+// console.log(new_md.toTimeString())
+let new_md1 = new Date(2023,11,23,3,70)
+// console.log(new_md1.toTimeString())
+// console.log(new_md1.toString())
+let new_md2 = new Date('1971-10-20')
+console.log(new_md2.toString())

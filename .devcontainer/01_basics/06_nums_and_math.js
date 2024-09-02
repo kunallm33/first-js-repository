f7c6d365c2b26b2572d718f462138e42505d2b21
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-09-02 08:32:04 +0000

    math and numbers

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+// const value = new Number(100)
+
+// console.log(value);
+
+// let value1 = 57.956
+// newval = value1.toString()
+// console.log(typeof newval);
+
+// console.log(value1.toFixed(3));
+
+// console.log(value1.toPrecision(2));
+
+// let value2  = 3455555555533333
+// console.log(value2.toLocaleString('en-IN'));
+
+let value3 = -2.34
+console.log(Math);
+console.log(Math.round(value3));
+console.log(Math.abs(value3));
+console.log(Math.ceil(value3));
+console.log(Math.floor(value3));
+
+// console.log(round(value3));
+arr = [4,5,2,4,2]
+console.log(Math.min(3,4,2,2,4,5))
+console.log(Math.max(3,4,2,2,4,5))
+
+console.log(Math.random())
+console.log((Math.random()*100))
+console.log((Math.random()*100)+1)
+
+
+
+
+
+

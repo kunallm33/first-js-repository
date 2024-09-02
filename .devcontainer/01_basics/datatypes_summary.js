6fe89a34203d5983cde226352db4c6f5d0df9db2
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-09-02 08:31:46 +0000

    datatypes in js

--- .devcontainer/01_basics/datatypes_summary.js
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -16,5 +16,15 @@ const anotherId = Symbol(123)
 
 console.log(id === anotherId)
 
+let bgNumber = 4334323232334n
+
 // nonprimitive
 
+arr = [3,4,6,2]
+
+let newfunc = function(){
+    
+    
+}
+newval = 40/+0
+// console.log(newval);

a3cfd9e47c66c8b8482f29bac93a85581a939d82
kunallm33 <132350653+kunallm33@users.noreply.github.com>
2024-09-02 08:31:23 +0000

    memory

---
+++ .devcontainer/01_basics/05_memorys.js
@@ -0,0 +1,26 @@
+// Non-primitive
+
+let new_use = {
+    name:'xyz',
+    int:87
+}
+
+let another_use = new_use
+
+another_use.name = 'gana'
+
+console.log(new_use);
+
+console.log(another_use);
+
+// Primitive
+
+gta6 = 'good graphics'
+
+gta5 = gta6
+
+gta5 = 'poor graphics'
+
+console.log(gta5);
+console.log(gta6);
+
